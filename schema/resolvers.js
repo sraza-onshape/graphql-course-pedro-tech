beb03bbdbe6a4ee42f33523f215f3b603275f8b5
Zain Raza <sraza@ptc.com>
2023-01-24 10:43:18 -0500

    add a query field + resolver that requires and parameter

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,4 +1,5 @@
 const { UserList } = require("../datasources/fake-data");
+const lodash = require("lodash");
 
 const resolvers = {
     // all the types 
@@ -8,6 +9,13 @@ const resolvers = {
             // JS goes here!! e.g. calls to retrieve db records
             return UserList;
         },
+        // this is another example that uses arrow syntax AND has an argument
+        user: (_, args) => {  // note: the first arg is parent
+            const id = args.id;  // should be a User.ID
+            // if you have a db --> use it here - but for now let's search via lodash
+            const user = lodash.find(UserList, { id: Number(id) });
+            return user;
+        }
     },
 };
 

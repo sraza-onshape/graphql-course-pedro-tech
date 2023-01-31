95639b13986646bb8d1e642a109b55d6a437e76f
Zain Raza <sraza@ptc.com>
2023-01-31 10:03:08 -0500

    set up architecture of GQL-React integration using apollo-client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  ApolloClient,  // each instance of this lets the client know you're connecting to an GQL API
+  InMemoryCache,
+  ApolloProvider,
+  // useQuery - TODO[Zain] - use this below!!
+} from '@apollo/client';
 
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "http://localhost:4000/graphql", // the URL where the GQL API is running
+  });
+  // This is an example of how to make an GQL query from a front-end app like React! 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>List of Users TBD</h1>
+      </div>
+    </ApolloProvider>
   );
 }
 
